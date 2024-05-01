1fb36c78a371440af08265b1cc9ec6fddb1ee52a
Konstantin Sorokin <61raven61@mail.ru>
2024-05-01 21:37:15 +0300

    added start and help commands

--- src/main.py
+++ src/main.py
@@ -3,12 +3,14 @@ import asyncio
 import logging
 
 from config import get_settings
+from handlers import router
 
 
 async def main():
     settings = get_settings()
 
     dp = Dispatcher()
+    dp.include_router(router=router)
 
     bot = Bot(
         token=settings.telegram.token.get_secret_value(),
